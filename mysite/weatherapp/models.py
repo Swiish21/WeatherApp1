f96a3f702613619baa0bac2a47ceec316388cf7d
Swiiish21 <malcolmotieno@gmail.com>
2024-10-11 09:51:14 +0300

    added info to be input in the django db(models.py)

--- mysite/weatherapp/models.py
+++ mysite/weatherapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+
+
 
 # Create your models here.
+class Member(models.Model):
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
